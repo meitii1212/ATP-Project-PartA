9536b3bea616c6f4939b69cbf771b0c88d91b1d7
meitii1212 <meitalgl@post.bgu.ac.il>
2021-04-03 21:12:18 +0300

    update full implementation of MazeState

--- algorithms/search/MazeState.java
+++ algorithms/search/MazeState.java
@@ -1,4 +1,29 @@
 package algorithms.search;
 
-public class MazeState {
+import algorithms.mazeGenerators.Position;
+
+public class MazeState extends AState{
+
+    //inherit all the functions of the AState class with dependency injection of Position
+    public MazeState(Position pos_arg) {
+        super(pos_arg);
+        this.origin = (Position) this.origin; //casting the object to exact type???
+        
+    }
+
+
+    //implementation of the abstruct functions:
+
+    public String toString(){
+       return ((Position)this.getOrigin()).toString();
+    }
+    public boolean equals(Object o){
+        Position p = (Position) o;
+        return ((Position)(this.getOrigin())).equals(p);
+    }
+    public int hashCode(){
+       return ((Position)this.getOrigin()).hashCode();
+    }
+
+
 }

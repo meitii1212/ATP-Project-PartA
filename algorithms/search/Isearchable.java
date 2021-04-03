16fcff4e62090bc074c6dd77c07450883b3cef70
meitii1212 <meitalgl@post.bgu.ac.il>
2021-04-03 21:01:55 +0300

    implementation of Isearchable functions

--- algorithms/search/Isearchable.java
+++ algorithms/search/Isearchable.java
@@ -1,4 +1,18 @@
 package algorithms.search;
 
+import java.util.ArrayList;
+
 public interface Isearchable {
+
+
+    //according to the interface in the lecture
+
+    AState getStartState();
+    AState getGoalState();
+    ArrayList<AState> getAllSuccessors(AState my_state);
+
+
+
+
+
 }

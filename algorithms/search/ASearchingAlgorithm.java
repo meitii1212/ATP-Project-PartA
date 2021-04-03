7420f6f4e9bf07f7d4a7325540aae7eb55dc44df
meitii1212 <meitalgl@post.bgu.ac.il>
2021-04-03 20:57:44 +0300

    update interface of ASearchingAlgorithm

--- algorithms/search/ASearchingAlgorithm.java
+++ algorithms/search/ASearchingAlgorithm.java
@@ -1,4 +1,43 @@
 package algorithms.search;
 
-public abstract class ASearchingAlgorithm {
+public abstract class ASearchingAlgorithm implements ISearchingAlgorithm {
+
+    private String Name; //algorithm name
+    private int NumberOfNodesEvaluated; //number of visited states we pass through reaching the final solution
+    private Isearchable problem;  //the specific problem we want to solve.
+    //public abstract Solution solve(Isearchable domain);
+
+    public ASearchingAlgorithm(Isearchable problem) {
+
+        this.problem = problem;
+    }
+
+
+    //SETTER AND GETTER
+
+    @Override
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    @Override
+    public int getNumberOfNodesEvaluated() {
+        return NumberOfNodesEvaluated;
+    }
+
+    public void setNumberOfNodesEvaluated(int numberOfNodesEvaluated) {
+        NumberOfNodesEvaluated = numberOfNodesEvaluated;
+    }
+
+    public Isearchable getProblem() {
+        return problem;
+    }
+
+    public void setProblem(Isearchable problem) {
+        this.problem = problem;
+    }
 }

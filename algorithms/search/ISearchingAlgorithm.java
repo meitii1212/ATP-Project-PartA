782480f7c1ef188fc521c3879c92df0d42597530
meitii1212 <meitalgl@post.bgu.ac.il>
2021-04-03 21:02:17 +0300

    implementation of ISearchingAlgorithm functions

--- algorithms/search/ISearchingAlgorithm.java
+++ algorithms/search/ISearchingAlgorithm.java
@@ -1,4 +1,11 @@
 package algorithms.search;
 
 public interface ISearchingAlgorithm {
+
+    //according to the lecture
+    Solution solve(Isearchable domain);
+    String getName();
+    int getNumberOfNodesEvaluated();
+
+
 }

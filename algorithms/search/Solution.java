f2520940b374d1ab90420104cb59e67cd07f0a2c
meitii1212 <meitalgl@post.bgu.ac.il>
2021-04-03 21:15:53 +0300

    missing full implementation for Solution

--- algorithms/search/Solution.java
+++ algorithms/search/Solution.java
@@ -1,4 +1,14 @@
 package algorithms.search;
 
+import java.util.ArrayList;
+
 public class Solution {
-}
+
+    private ArrayList<AState> SolutionPath;
+
+
+    //getter!
+    //public ArrayList<AState> getSolutionPath(){
+
+    }
+

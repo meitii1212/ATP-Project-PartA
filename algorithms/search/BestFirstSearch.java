ebdfaa513767474d28ae53c9df8e5a47df6fb55c
meitii1212 <meitalgl@post.bgu.ac.il>
2021-04-05 01:00:18 +0300

    BestFirstSearch - full implementation before testing by inheritance

--- algorithms/search/BestFirstSearch.java
+++ algorithms/search/BestFirstSearch.java
@@ -1,4 +1,24 @@
 package algorithms.search;
 
-public class BestFirstSearch {
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * this algorithm active the same as the BreadthFirstSearch but it usues priority queue
+ * in order to find the cheepest path but it doesn't have to be the shortest path
+ */
+public class BestFirstSearch extends BreadthFirstSearch{
+
+    public BestFirstSearch(Isearchable problem) {
+        super(problem);
+        setName("BestFirstSearch");
+        AStateComperator my_comp = new AStateComperator(); //comparing by the cost of the AState
+        queue = new PriorityQueue<AState>(my_comp);
+
+    }
+
+
+
+
+
 }

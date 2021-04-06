9f6188d9a9bac5a8dd5eabd9434a05f597aa44c6
meitii1212 <meitalgl@post.bgu.ac.il>
2021-04-06 15:19:05 +0300

    ISearchable instead of Isearchable

---
+++ algorithms/search/ISearchable.java
@@ -0,0 +1,16 @@
+package algorithms.search;
+
+import java.util.ArrayList;
+
+public interface ISearchable {
+
+
+    //according to the interface in the lecture
+
+    AState getStartState();
+
+    AState getGoalState();
+
+    ArrayList<AState> getAllSuccessors(AState my_state);
+
+}

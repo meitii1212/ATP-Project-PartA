f5a6ff895b53a507cd0aafd146620928834b24e5
meitii1212 <meitalgl@post.bgu.ac.il>
2021-04-06 15:18:44 +0300

    AStateComperator - added for the priority queue

---
+++ algorithms/search/AStateComperator.java
@@ -0,0 +1,22 @@
+package algorithms.search;
+
+import java.util.Comparator;
+
+public class AStateComperator implements Comparator<AState>{
+
+    /**
+     * @param o1 AState 1 with cost
+     * @param o2 AState 2 with cost
+     * @return  calls the compare cost function of AState
+     */
+        @Override
+        public int compare(AState o1, AState o2) {
+            return o1.CompareCost(o2);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return false;
+        }
+
+}

ede6bfdbfa1cacfeaaece884feb0dacb2abfec1b
meitii1212 <meitalgl@post.bgu.ac.il>
2021-04-06 19:05:41 +0300

    DFS - complete after basic tests +adding hashmap of the succesors

--- algorithms/search/DepthFirstSearch.java
+++ algorithms/search/DepthFirstSearch.java
@@ -1,8 +1,6 @@
 package algorithms.search;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * implementation for the iterative DFS algorithm.
@@ -11,17 +9,19 @@ public class DepthFirstSearch extends ASearchingAlgorithm {
 
     private Stack<AState> stack;
     private HashSet<AState> VisitedSet;
-
+    private HashMap<AState,AState> FoundSuccessors; //all the successors we found throght the searching maaped to their parent
     /**CONSTRUCTOR*/
     public DepthFirstSearch() {
         super();
         setName("DepthFirstSearch");
-        Stack<AState> stack = new Stack<>(); //the stack that collect the possibloe next steps
-        HashSet<AState> VisitedSet = new HashSet<>();// a hash set that keeps all the visited states
+
+
     }
 
     public Solution solve(ISearchable domain){
-
+        stack = new Stack<>(); //the stack that collect the possibloe next steps
+        VisitedSet = new HashSet<>();// a hash set that keeps all the visited states
+        FoundSuccessors = new HashMap<AState,AState>();
         //creating the solution object that will be returned
         problem = domain;
         Solution final_solution = new Solution();
@@ -31,6 +31,7 @@ public class DepthFirstSearch extends ASearchingAlgorithm {
 
         //pushing the StartState to the stack
         stack.push(curr);
+        FoundSuccessors.put(curr,null);
 
         //loop on the stack until there are no more AStates
         while(!stack.empty()){
@@ -50,16 +51,30 @@ public class DepthFirstSearch extends ASearchingAlgorithm {
 
             //if we arrived to the goal state
             if(curr.equals(goal)) {
+
                 //NumberOfNodesEvaluated++
                 this.AddsEvaluatedNode();
-                final_solution.setSolutionPath(goal);
+                goal.setParent(FoundSuccessors.get(goal));
+                final_solution.BuildSolutionPath(goal);
                 return final_solution;
             }
             //if its not the goal
             ArrayList<AState> Neighbors = problem.getAllSuccessors(curr);
             for (int i = 0; i < Neighbors.size(); i++) {
                 if (!(VisitedSet.contains(Neighbors.get(i)))){
-                    Neighbors.get(i).setParent(curr);
+
+                    //if neighbor not in successors-> add it with the updated parent
+                    if(!FoundSuccessors.containsKey(Neighbors.get(i))) {
+                        Neighbors.get(i).setParent(curr);
+                        FoundSuccessors.put(Neighbors.get(i),curr);
+
+                    }
+                    //if its it the table, update the correct parent
+                    else{
+                        Neighbors.get(i).setParent(FoundSuccessors.get(Neighbors.get(i)));
+
+                    }
+
                     stack.push(Neighbors.get(i));
                 }
 

49bc83b120ef0a0892ee3d15f67c648e6e8289db
meitii1212 <meitalgl@post.bgu.ac.il>
2021-04-06 19:05:22 +0300

    Breadth - complete after basic tests +adding hashmap of the succesors

--- algorithms/search/BreadthFirstSearch.java
+++ algorithms/search/BreadthFirstSearch.java
@@ -1,9 +1,6 @@
 package algorithms.search;
 
-import java.util.AbstractQueue;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * BFS algorithem implementation in order to find the shortest path in the Isearchable prolem
@@ -13,33 +10,37 @@ public class BreadthFirstSearch extends ASearchingAlgorithm {
     protected AbstractQueue<AState> queue; //queue of all the possible steps that available in the current isearchable.
 
     HashSet<AState> VisitedSet;
+    private HashMap<AState,AState> FoundSuccessors; //all the successors we found throght the searching maaped to their parent
+
 
     //CONSTRUCTOR
     public BreadthFirstSearch() {
         super();
         setName("BreadthFirstSearch");
-        AbstractQueue<AState> queue = new Queue();
-        HashSet<AState> VisitedSet = new HashSet<>();// a hash set that keeps all the visited states
     }
 
     public Solution solve(ISearchable domain){
         problem = domain;
+        AbstractQueue<AState> queue = new Queue();
+        HashSet<AState> VisitedSet = new HashSet<>();// a hash set that keeps all the visited states
+        FoundSuccessors = new HashMap<AState,AState>(); //a hash table that map each neighbor we revealed to his parent
         Solution final_solution = new Solution();
         AState start = problem.getStartState();
         AState goal = problem.getGoalState();
 
         queue.offer(start);
+        FoundSuccessors.put(start,null);
         while (queue.size() != 0){
             AState curr = queue.poll();
             //if we reached the goal
             if(curr.equals(goal)){
 
-                // add the goal to the visited list
+                // add the goal to the visited list and update its parent
                 VisitedSet.add(curr);
-
+                goal.setParent(FoundSuccessors.get(curr));
                 //NumberOfNodesEvaluated++
                 this.AddsEvaluatedNode();
-                 final_solution.setSolutionPath(goal);
+                 final_solution.BuildSolutionPath(goal);
                  return final_solution;
             }
 
@@ -52,10 +53,20 @@ public class BreadthFirstSearch extends ASearchingAlgorithm {
                     if(!(VisitedSet.contains(Neighbors.get(i)))){
                         //add the  neighbor to the visited set and update the queue
                         VisitedSet.add(Neighbors.get(i)) ;
-                        //NumberOfNodesEvaluated++
-                        this.AddsEvaluatedNode();
-                        Neighbors.get(i).setParent(curr);
+                        //if neighbor not in successors-> add it with the updated parent
+                        if(!FoundSuccessors.containsKey(Neighbors.get(i))) {
+                            Neighbors.get(i).setParent(curr);
+                            FoundSuccessors.put(Neighbors.get(i), curr);
+                            //NumberOfNodesEvaluated++
+                            this.AddsEvaluatedNode();
+                        }
+                        //if its it the table, update the correct parent
+                        else{
+                            Neighbors.get(i).setParent(FoundSuccessors.get(Neighbors.get(i)));
+
+                        }
                         queue.offer(Neighbors.get(i));
+
                     }
 
             }

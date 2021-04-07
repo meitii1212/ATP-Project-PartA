816c8e92007217c022467faa2c9444762578416f
bargold100 <goldkbar@post.bgu.ac.il>
2021-04-07 17:38:57 +0300

    insert AMaze3DGenerator

--- algorithms/maze3D/AMaze3DGenerator.java
+++ algorithms/maze3D/AMaze3DGenerator.java
@@ -1,4 +1,12 @@
 package algorithms.maze3D;
 
-public abstract class AMaze3DGenerator {
+public abstract class AMaze3DGenerator implements IMaze3DGenerator {
+
+    public long measureAlgorithmTimeMillis(int depth, int row, int column){
+
+        long first_time = System.currentTimeMillis();
+        this.generate(depth,row,column);
+        long last_time = System.currentTimeMillis();
+        return last_time- first_time;
+    }
 }

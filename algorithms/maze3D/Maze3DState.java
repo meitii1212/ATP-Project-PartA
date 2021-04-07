c800847b2c8602aa45fc47a3c834be0e6598944b
bargold100 <goldkbar@post.bgu.ac.il>
2021-04-07 17:40:09 +0300

    insert Maze3DState

--- algorithms/maze3D/Maze3DState.java
+++ algorithms/maze3D/Maze3DState.java
@@ -1,4 +1,37 @@
 package algorithms.maze3D;
 
-public class Maze3DState {
+import algorithms.mazeGenerators.Position;
+import algorithms.search.AState;
+import algorithms.search.MazeState;
+
+public class Maze3DState extends AState {
+
+    //inherit all the functions of the AState class with dependency injection of Position
+    //CONSTRUCTOR
+    public Maze3DState(Position3D pos3d_arg) {
+        super(pos3d_arg);
+        this.origin = (Position3D) this.origin;
+    }
+
+    //implementation of the abstruct functions:
+    @Override
+    public String toString() {
+
+            return ((Position3D)this.getOrigin()).toString();
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Maze3DState p = (Maze3DState) o;
+        return ((Position3D)(this.getOrigin())).equals(p.getOrigin());
+    }
+
+    @Override
+    public int hashCode() {
+        return ((Position3D)this.getOrigin()).hashCode();
+
+    }
 }

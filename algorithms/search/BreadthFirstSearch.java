f1f4f06318fc9efbf272786c3e6027cedc02fcef
meitii1212 <meitalgl@post.bgu.ac.il>
2021-04-18 01:18:42 +0300

    BreadthFS - correcting algorithm

--- algorithms/search/BreadthFirstSearch.java
+++ algorithms/search/BreadthFirstSearch.java
@@ -8,77 +8,93 @@ import java.util.*;
 public class BreadthFirstSearch extends ASearchingAlgorithm {
 
     protected AbstractQueue<AState> queue; //queue of all the possible steps that available in the current isearchable.
-
+    protected boolean Flag = true;
     HashSet<AState> VisitedSet;
-    private HashMap<AState,AState> FoundSuccessors; //all the successors we found throght the searching maaped to their parent
+    private HashMap<AState, AState> FoundSuccessors; //all the successors we found throght the searching maaped to their parent
 
 
     //CONSTRUCTOR
     public BreadthFirstSearch() throws Exception {
         super();
+        queue = new Queue();
         setName("BreadthFirstSearch");
     }
 
     public Solution solve(ISearchable domain) throws Exception {
         NullArgCheck(domain);
         problem = domain;
-        AbstractQueue<AState> queue = new Queue();
+
         HashSet<AState> VisitedSet = new HashSet<>();// a hash set that keeps all the visited states
-        FoundSuccessors = new HashMap<AState,AState>(); //a hash table that map each neighbor we revealed to his parent
+        FoundSuccessors = new HashMap<AState, AState>(); //a hash table that map each neighbor we revealed to his correct cost and parent
         Solution final_solution = new Solution();
         AState start = problem.getStartState();
         AState goal = problem.getGoalState();
-
         queue.offer(start);
-        FoundSuccessors.put(start,null);
-        while (queue.size() != 0){
+        start.setParent(null);
+        FoundSuccessors.put(start, start);
+        VisitedSet.add(start); //adding the start to the visited nodes
+        while (queue.size() != 0) {
             AState curr = queue.poll();
             //if we reached the goal
-            if(curr.equals(goal)){
+            if (curr.equals(goal)) {
 
                 // add the goal to the visited list and update its parent
                 VisitedSet.add(curr);
-                goal.setParent(FoundSuccessors.get(curr));
+                goal.setParent((FoundSuccessors.get(curr)).getParent());
                 //NumberOfNodesEvaluated++
                 this.AddsEvaluatedNode();
-                 final_solution.BuildSolutionPath(goal);
-                 return final_solution;
+                final_solution.BuildSolutionPath(goal);
+                return final_solution;
             }
 
             //checking the neighbors of the curr state if there are still
             //neighbors to visit.
-            ArrayList<AState> Neighbors = problem.getAllSuccessors(curr);
-            for(int i = 0; i < Neighbors.size(); i++) {
 
+            int flag = 0; //flag to identify whether one of my neighbors is the goal
+            ArrayList<AState> Neighbors = problem.getAllSuccessors(curr);
+
+            // a loop to search if one of the neibors is the goal and if we are in case that need to consider the costs like BestFS
+            for (int i = 0; i < Neighbors.size(); i++) {
+                if (Neighbors.get(i).equals(goal)&& Flag==false) {
+                    flag = 1;
+                }
                 //if we reached unvisited neighbor
-                    if(!(VisitedSet.contains(Neighbors.get(i)))){
-                        //add the  neighbor to the visited set and update the queue
-                        VisitedSet.add(Neighbors.get(i)) ;
+                if (!(VisitedSet.contains(Neighbors.get(i))) || flag == 1) {
+
+                    if (!FoundSuccessors.containsKey(Neighbors.get(i)) || flag == 1) {
+                        // if one of the neighbors is the cost and we need to consider that- update the details  of the goal in the hashmap.
+                        if (flag == 1 &&FoundSuccessors.containsKey(Neighbors.get(i)) ) {
+                            if ((Neighbors.get(i).getCost() + curr.getCost()) < (FoundSuccessors.get(Neighbors.get(i)).getCost())) {
+                                Neighbors.get(i).setCost(Neighbors.get(i).getCost() + curr.getCost());
+                                Neighbors.get(i).setParent(curr);
+                                FoundSuccessors.replace(Neighbors.get(i), Neighbors.get(i));
+
+                            }
+                        }
                         //if neighbor not in successors-> add it with the updated parent
-                        if(!FoundSuccessors.containsKey(Neighbors.get(i))) {
+                        else {
                             Neighbors.get(i).setParent(curr);
-                            FoundSuccessors.put(Neighbors.get(i), curr);
+                            Neighbors.get(i).setCost(Neighbors.get(i).getCost() + curr.getCost());
+                            FoundSuccessors.put(Neighbors.get(i), Neighbors.get(i));
                             //NumberOfNodesEvaluated++
                             this.AddsEvaluatedNode();
+                            queue.offer(Neighbors.get(i));
                         }
-                        //if its it the table, update the correct neighbor to its original father
-                        else{
-                            Neighbors.get(i).setParent(FoundSuccessors.get(Neighbors.get(i)));
 
-                        }
-                        queue.offer(Neighbors.get(i));
-
+                        //add the  neighbor to the visited set and update the queue
+                        VisitedSet.add(Neighbors.get(i));
+                        flag = 0;
                     }
 
+                }
             }
-        }
 
+
+
+        }
         //if there is no solution to the path - not suppose to happen return empty solution
         return final_solution;
 
 
     }
-
-
-
 }

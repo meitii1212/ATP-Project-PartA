dcf56a9460bcb1da640bf4240968b631c09857f4
bargold100 <goldkbar@post.bgu.ac.il>
2021-05-04 18:09:42 +0300

    update Client package

--- Client/IClientStrategy.java
+++ Client/IClientStrategy.java
@@ -1,4 +1,9 @@
 package Client;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public interface IClientStrategy {
+    public void applyStrategy(InputStream inFromServer, OutputStream outToServer);
+
 }
